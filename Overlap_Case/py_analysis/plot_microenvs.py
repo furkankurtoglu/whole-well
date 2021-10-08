3d1201227b284352b8c1fb0886bce97d7d151b8a
Kali <kalikons7@gmail.com>
2021-10-08 13:52:07 -0400

    Overlap Case results

--- Overlap_Case/py_analysis/plot_microenvs.py
+++ Overlap_Case/py_analysis/plot_microenvs.py
@@ -19,8 +19,8 @@ import matplotlib.animation
 
 import numpy as np
 import pandas as pd
-from fury import window, actor, utils, primitive, io, ui
-from fury.data import read_viz_textures, fetch_viz_textures
+#from fury import window, actor, utils, primitive, io, ui
+#from fury.data import read_viz_textures, fetch_viz_textures
 import itertools
 import vtk
 import glob
